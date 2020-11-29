32de5e62647115917a9e2e6f6bbc328be254d27a
lam2003 <admin@linmin.xyz>
2020-11-29 23:53:38 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "http/http_request_splitter.h"
 using namespace sms;
 
-void signal_handler(int signo){}
+void signal_handler(int signo) {}
 
 int main(int argc, char **argv)
 {
@@ -34,16 +34,26 @@ int main(int argc, char **argv)
     g_sig_hdl.AddSignal(SIGTERM);
     g_sig_hdl.AddSignal(SIGINT);
     g_sig_hdl.AddSignal(SIGPIPE);
-    char buf[2 * 1024 * 1024] = {1};
+    char buff[2 * 1024 * 1024] = {1};
+    std::shared_ptr<BufferRaw> buf = std::make_shared<BufferRaw>();
+
+    buf->Assign(buff, 2 * 1024 * 1024);
     tcp_server.Start(reinterpret_cast<uv_tcp_t *>(SocketUtils::Bind(SocketUtils::SOCK_TCP, "0.0.0.0", 80)), 1024);
     tcp_server.SetAcceptCB([buf](TcpConnection *cc) {
+        std::list<std::shared_ptr<Buffer>> ll;
+        ll.push_back(buf);
+        ll.push_back(buf);
+        ll.push_back(buf);
+        ll.push_back(buf);
+        ll.push_back(buf);
         // LOG_I << cc->GetPeerPort() << " connected";
         cc->SetReadCB([](TcpConnection *cc, const uint8_t *data, size_t len) {
-            cc->Close();
+            // cc->Close();
             // HttpRequestSplitter ss;
             // HttpParser parser;
 
-            // return ss.Input(data, len);
+            // ss.Input(data, len);
+            cc->Close();
             return len;
             // parser.Process(std::string(reinterpret_cast<const char *>(data), len));
             // std::string str(reinterpret_cast<const char *>(data), len);
@@ -51,12 +61,14 @@ int main(int argc, char **argv)
             // return len;
         });
 
-        cc->Write(reinterpret_cast<const uint8_t *>(buf), 2 * 1024 * 1024, [](bool v) {
-            // LOG_E << "####################################### " << v;
+        cc->Write(std::make_shared<BufferList>(ll), [cc](bool v) {
+            LOG_E << "####################################### 1 " << v << "   " << cc->GetPeerPort();
         });
+
+  
     });
     tcp_server.SetClosedCB([](TcpConnection *cc) {
-        // LOG_D << cc->GetPeerPort() << " closed";
+        LOG_D << cc->GetPeerPort() << " closed";
     });
 
     DepLibUV::RunLoop();
